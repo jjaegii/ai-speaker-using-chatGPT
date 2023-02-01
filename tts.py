3d201b3ba7d6ae7b7808f4649229aa07069a2fc0
jjaegii <hn06038@gmail.com>
2023-02-01 08:53:49 +0000

    for raspberryPi

--- tts.py
+++ tts.py
@@ -1,11 +1,15 @@
-import speech_recognition as sr
 from gtts import gTTS
-import playsound
+import pygame
 import os
 
 def speak(text):
      tts = gTTS(text=text, lang='ko')
      filename='voice.mp3'
      tts.save(filename)
-     playsound.playsound(filename)
+     pygame.mixer.init()
+     pygame.mixer.music.load("voice.mp3")
+     pygame.mixer.music.play()
+     while pygame.mixer.music.get_busy() == True:
+          continue     
      os.remove('voice.mp3')
+
