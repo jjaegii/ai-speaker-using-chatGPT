def74dc6490b08ab184ee0aa4d1256db7f629b5d
jjaegii <hn06038@gmail.com>
2023-02-03 16:12:58 +0900

    add chatGPT function

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request, jsonify
+import chat
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def a():
-    return render_template('test.html')
+    return render_template('speaker.html')
+
 
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
     if request.method == 'POST':
         prompt = request.json
         print(prompt)
-    return jsonify({"hello":prompt})
+        answer = chat.ask(prompt)
+        print(answer)
+        return jsonify({"answer": answer})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
